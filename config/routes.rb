2044c9d17d8cfd7e3668fe25de8fb8706118ee63
Blane <blane.cordes@gmail.com>
2011-01-25 21:02:06 -0500

    added factories and test for show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+ 
+ resources :users
+ 
+ root :to => 'pages#home'
 
-  match '/signup',  :to => 'users#new'
-  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
-  root :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+ 
 
-end
 
 
