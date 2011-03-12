ae5c63f25638cbcad3899e39dcd45c525bebef96
Blane <blane.cordes@gmail.com>
2011-03-12 15:58:47 -0500

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@
   attr_accessible :name, :email, :password, :password_confirmation
   
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                            :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -28,15 +35,25 @@
         (user && user.salt == cookie_salt) ? user : nil
       end
     
+      def following?(followed)
+        relationships.find_by_followed_id(followed)
+      end
+
+      def follow!(followed)
+        relationships.create!(:followed_id => followed.id)
+      end
+      
+      def unfollow!(followed)
+          relationships.find_by_followed_id(followed).destroy
+        end
     
     def has_password?(submitted_password)
       encrypted_password == encrypt(submitted_password)
     end
     
     def feed
-      # This is preliminary. See Chapter 12 for the full implementation.
-      Micropost.where("user_id = ?", id)
-    end
+        Micropost.from_users_followed_by(self)
+      end
     
   class << self  
     def User.authenticate(email, submitted_password)
